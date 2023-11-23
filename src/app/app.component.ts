b8187e99fc189d06c57310a92b2de483d3f0656d
mai <zrodriguezperez@hotmail.com>
2023-11-23 13:20:32 +0100

    host pseudo selector

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Course } from './models/Course.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  courses: Course[]=[
+    {title: 'Angular Complete Course', isActive:false},
+    {title: 'Angular Unit Testing', isActive:false},
+    {title: 'Angular RxJs', isActive:false},
+  ];
+  title = 'mation';
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CourseComponent } from './Components/course/course.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CourseComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
