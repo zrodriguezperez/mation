3eb58a3de2bbfafa4dbb93f17a54e4e86515e8b7
mai <zrodriguezperez@hotmail.com>
2023-11-26 12:09:53 +0100

    emulated dom

--- src/app/Components/course/course.component.ts
+++ src/app/Components/course/course.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { Course } from 'src/app/models/Course.model';
 
 @Component({
   selector: 'app-course',
   templateUrl: './course.component.html',
-  styleUrls: ['./course.component.css']
+  styleUrls: ['./course.component.css'],
+  encapsulation: ViewEncapsulation.ShadowDom,
 })
 export class CourseComponent {
    @Input() course!: Course;
